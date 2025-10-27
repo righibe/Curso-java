080e508102413c76fa8f8706b4400c18f70844ea
righibe <seuemail@exemplo.com>
2025-10-27 15:32:46 -0300

    estudei sobre métodos e parametros tipo referencia

--- src/cursojava/javacore/Bintroducao/test/CalculadoraTeste03.java
+++ src/cursojava/javacore/Bintroducao/test/CalculadoraTeste03.java
@@ -5,7 +5,14 @@ import cursojava.javacore.Bintroducao.dominio.Calculadora;
 public class CalculadoraTeste03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        double result = calculadora.divideDoisNumeros(10 , 2);
+        double result = calculadora.divideDoisNumeros(10, 0);
         System.out.println(result);
+
+        System.out.println(calculadora.divideDoisNumeros02DS(15 , 0));
+        System.out.println("________________________________");
+
+
+        calculadora.imprimeDivisaoDeDoisnumeros(10,30);
+
     }
 }
